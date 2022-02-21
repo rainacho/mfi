8ed0aa0b874476fe14b4718c55545eefc9af5dc1
Raina Cho <chohr82@gmail.com>
2022-02-21 21:46:30 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+	var docheight;
+    var docwidth;
+
+	$('.my-conv-form-wrapper').convform({selectInputStyle: 'disable'});
+
+	function setSize(){
+		
+		//form style custom
+		docheight = window.innerHeight;
+		docwidth = window.innerWidth;
+
+		$('form.convFormDynamic > textarea.userInputDynamic').css({
+			'width' : docwidth - 66,
+		});
+		$('.wrapper-messages').css({
+			'min-height':docheight - 48,
+			'height':docheight,
+			
+		});
+
+		$('div.conv-form-wrapper div#messages').css({
+			'min-height':'180px',
+			'height':docheight,
+			'max-height': docheight - 45
+		})
+	}
+
+	$('.convFormDynamic').addClass('type2');
+
+	$('form.convFormDynamic > textarea.userInputDynamic').css({
+		'margin':'10px',
+        'max-width':'unset',
+		'height': '40px',
+		'font-size' : '1.1rem',
+		'padding-top': '10px'
+	});
+
+	$('form.convFormDynamic > button.submit').css({
+		'background':'whitesmoke',
+		'height' : '40px',
+		'margin-top' : '10px',
+		'color' : '#02a5af' 
+	});
+
+	$('div.conv-form-wrapper div.options div.option').css('font-size','1.1rem');
+
+
+	$('#userInput').attr('placeholder','여기에 입력해 주세요');
+
+	$(window).resize(function(){
+		setSize();
+		console.log('resize');
+	});
+
+	//init set size
+	setSize();
+	
+});
